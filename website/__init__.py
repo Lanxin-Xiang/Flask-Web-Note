edacb08c0db37fc2b7d25faeab0f6b239b083e83
Lanxin-Xiang <91483229+Lanxin-Xiang@users.noreply.github.com>
2022-08-14 10:52:46 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -13,6 +13,9 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+
+
+
     from .views import views
     from .auth import auth
 
@@ -23,13 +26,14 @@ def create_app():
 
     create_database(app)
 
+    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(id):
-        return User.query.get(int(id))
+        return User.query.get(int(id)) #primary key
 
     return app
 
